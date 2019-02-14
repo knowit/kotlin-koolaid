681f5a9a575d165b0bede8e69d53017f2613378f
Henning Lund-Hanssen <henlun@knowit.no>
2019-02-14 17:08:23 +0100

    ktor-example: Lagt til Ktor eksempel som kompilerer! NB! Må ha med jcenter repo

--- ktor-example/build.gradle.kts
+++ ktor-example/build.gradle.kts
@@ -1,25 +1,38 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-plugins {
-    java
-    kotlin("jvm") version "1.3.21"
-}
-
-group = "no.knowit.jvm"
+group = "Example"
 version = "1.0-SNAPSHOT"
+
+val ktorVersion = "1.1.1"
+val logbackVersion = "1.2.3"
+val junitVersion = "5.3.1"
+
+plugins {
+    application
+    kotlin("jvm") version "1.3.11"
+}
 
 repositories {
     mavenCentral()
+    jcenter()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType<KotlinCompile>().all {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClassName = "MainKt"
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    testCompile("junit", "junit", "4.12")
-}
-
-configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-}
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    implementation(group = "io.ktor", name = "ktor-server-netty", version = ktorVersion)
+    implementation(group = "ch.qos.logback", name = "logback-classic", version = logbackVersion)
+    testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-api", version = junitVersion)
+    testRuntimeOnly(group = "org.junit.jupiter", name = "junit-jupiter-engine", version = junitVersion)
 }
